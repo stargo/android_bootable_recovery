fe7ced61e6c7d9fbf053edc6c34a8fabcd4aa63c
wangyou <1078360711@.qq.com>
2015-03-13 15:09:59 +0800

    edison：Safestrap edits for edison compatibility

---
+++ safestrap/devices/motorola/edison/build-install-kexec.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+cd $OUT
+rm $OUT/APP/install-files.zip
+rm $OUT/install-files/etc/safestrap/2nd-init.zip
+rm $OUT/install-files/etc/safestrap/ramdisk-recovery.img
+zip -9rj install-files/etc/safestrap/2nd-init 2nd-init-files/*
+
+cd $OUT/recovery/root
+# copy correct bbx and fixboot.sh
+cp $ANDROID_BUILD_TOP/bootable/recovery/safestrap/bbx ./sbin/bbx
+cp $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/common/2nd-init-files/fixboot.sh ./sbin/fixboot.sh
+cp $ANDROID_BUILD_TOP/bootable/recovery/safestrap/devices/motorola/edison/init.rc ./init.rc
+
+#kexec-files
+cp $ANDROID_BUILD_TOP/device/motorola/omap4-common/kexec/* $OUT/install-files/etc/safestrap/kexec/
+
+#kernel
+cp $OUT/kernel $OUT/install-files/etc/safestrap/kexec/kernel
